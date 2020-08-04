34ace5e2ccd64e9bd7ec59f5f8001166cb13afe4
Narongrit Rodphroboon <narongrit.rodphroboon@allianz.com>
2020-08-04 14:53:12 +0700

    Add method to getResult

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -2,22 +2,25 @@ public class TennisGame2
 {
     private int player1Point;
     private int player2Point;
-    private String player1Name;
-    private String player2Name;
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
     }
 
-    public String getScore(){
-        String[] textScores = {"Love", "Fifteen", "Thirty", "Forty"};
+    public String getResult(int point){
+        String[] textResult = {"Love", "Fifteen", "Thirty", "Forty"};
+        return textResult[point];
+    }
 
+    public String getScore(){
 //        Draw
         boolean drawNotDeuce = player1Point == player2Point && player1Point < 3;
         if (drawNotDeuce)
         {
-            return textScores[player1Point]+"-All";
+            return getResult(player1Point)+"-All";
         }
 
 //        Deuce
@@ -27,27 +30,30 @@ public class TennisGame2
         }
 
         //        Winner
-        if (player1Point >=4 && player2Point >=0 && (player1Point - player2Point)>=2)
+        boolean player1Winner = player1Point >= 4 && player2Point >= 0 && (player1Point - player2Point) >= 2;
+        if (player1Winner)
         {
             return "Win for "+player1Name;
         }
-        if (player2Point >=4 && player1Point >=0 && (player2Point - player1Point)>=2)
+        boolean player2Winner = player2Point >= 4 && player1Point >= 0 && (player2Point - player1Point) >= 2;
+        if (player2Winner)
         {
             return "Win for "+player2Name;
         }
 
 //        Advantage
-        if (player1Point > player2Point && player2Point >= 3)
+        boolean player1Advantage = player1Point > player2Point && player2Point >= 3;
+        if (player1Advantage)
         {
             return "Advantage "+player1Name;
         }
-        if (player2Point > player1Point && player1Point >= 3)
+        boolean player2Advantage = player2Point > player1Point && player1Point >= 3;
+        if (player2Advantage)
         {
             return "Advantage "+player2Name;
         }
 
-
-        return textScores[player1Point]+"-"+textScores[player2Point];
+        return getResult(player1Point)+"-"+getResult(player2Point);
     }
 
     public void player1Score(){
